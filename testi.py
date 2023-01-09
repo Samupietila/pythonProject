6d772603bf9a512fcadeb7a74f75f6c5ff97197b
Samupietila <122264320+Samupietila@users.noreply.github.com>
2023-01-09 11:19:20 +0200

    Initial commit

---
+++ testi.py
@@ -0,0 +1,5 @@
+print("Tervetuloa opiskelemaan Python-kieltä!")
+print("Tervetuloa opiskelemaan Python-kieltä!")
+print("Tervetuloa opiskelemaan Python-kieltä!")
+print("Tervetuloa opiskelemaan Python-kieltä!")
+print("Tervetuloa opiskelemaan Python-kieltä!")
