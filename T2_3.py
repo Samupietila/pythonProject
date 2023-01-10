a1d650a304b6a4874d41bf86700d76d7f6fcc1dd
Samupietila <122264320+Samupietila@users.noreply.github.com>
2023-01-10 11:22:19 +0200

    tehtäviä

---
+++ T2_3.py
@@ -0,0 +1,6 @@
+Kanta = float(input('Anna suorakulmion kannan pituus: '))
+Korkeus = float(input('Anna suorakulmion korkeus: '))
+Piiri = 2 * Kanta + 2 * Korkeus
+Pinta_ala = Kanta * Korkeus
+print('Suorakulmion piiri on', Piiri, 'ja sen pinta-ala on', Pinta_ala)
+print('Kiitos ohjelman käytöstä.')
