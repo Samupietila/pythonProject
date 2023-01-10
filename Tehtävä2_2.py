148b86b3532186ddb7ff2d17b8772b2881633fe5
Samupietila <122264320+Samupietila@users.noreply.github.com>
2023-01-10 12:49:43 +0200

    tehtäviä

--- Tehtävä2_2.py
+++ Tehtävä2_2.py
@@ -1,4 +1,4 @@
-Sade = float(input('Anna ympyrän säde :'))
+Sade = float(input('Anna ympyrän säde: '))
 pii = 3.14
-pinta_ala = 2 * Sade * pii
+pinta_ala = Sade * Sade * pii
 print('Ympyrän pinta-ala on', pinta_ala)
