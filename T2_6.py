e074501810dce6e49df4eb1a00bd576fd1a4bba6
Samupietila <122264320+Samupietila@users.noreply.github.com>
2023-01-10 16:32:09 +0200

    tehtäviä

---
+++ T2_6.py
@@ -0,0 +1,10 @@
+import random
+r1 = random.randint(0, 9)
+r2 = random.randint(0, 9)
+r3 = random.randint(0, 9)
+r4 = random.randint(1, 6)
+r5 = random.randint(1, 6)
+r6 = random.randint(1, 6)
+r7 = random.randint(1, 6)
+print('Kolminumeroinen numerolukko koodi',r1, r2, r3)
+print('Nelinumeroinen numerolukko koodi', r4, r5, r6, r7)
