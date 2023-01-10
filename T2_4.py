b49ee8706d4010047b5d485481abcca0f8782db0
Samupietila <122264320+Samupietila@users.noreply.github.com>
2023-01-10 11:28:12 +0200

    tehtäviä

---
+++ T2_4.py
@@ -0,0 +1,10 @@
+luku1 = int(input('Anna ensimmäinen kokonaisluku: '))
+luku2 = int(input('Anna toinen kokonaisluku: '))
+luku3 = int(input('Anna kolmas kokonaisluku: '))
+Summa = luku1 + luku2 + luku3
+Tulo = luku1 * luku2 * luku3
+Keskiarvo = Summa / 3
+print('Lukujen summa on', Summa)
+print('Lukujen tulo on', Tulo)
+print('Lukujen keskiarvo on', Keskiarvo)
+print('Kiitos ohjelman käytöstä.')
