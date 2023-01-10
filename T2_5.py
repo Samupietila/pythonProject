3c9dde9db041b419d00939378aa045ce702af872
Samupietila <122264320+Samupietila@users.noreply.github.com>
2023-01-10 16:08:50 +0200

    tehtäviä

--- T2_5.py
+++ T2_5.py
@@ -5,6 +5,13 @@ Luodit = float(input('Anna Luodit: '))
 Luoti = 13.3 * Luodit
 Naula = 13.3 * 32 * Naulat
 Leiviska = 20 * 32 * 13.3 * Leivat
-print(Naula, Leiviska, Luoti)
-print(Naula + Leiviska + Luoti)
+Yhteensa = Naula + Leiviska + Luoti
+Kilogrammat = Yhteensa / 1000
+Grammat = Kilogrammat - int(Kilogrammat)
+Grammat = Grammat * 1000
+Grammat = round(Grammat, 2)
+Kilogrammat = int(Kilogrammat)
+print('Massa nykymittojen mukaan:')
+print(Kilogrammat, 'kilogrammaa ja', Grammat, 'grammaa.')
+
 
