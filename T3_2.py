311f52935ffefff57ee4137e20ff060537f46c76
Samupietila <122264320+Samupietila@users.noreply.github.com>
2023-01-11 14:43:32 +0200

    tehtävä 1

---
+++ T3_2.py
@@ -0,0 +1,13 @@
+print('Tämä ohjelma kysyy käyttäjältä hyttiluokan ja antaa siitä kuvauksen.')
+Hyttiluokka = input('Anna hyttiluokka: ')
+if Hyttiluokka == 'LUX':
+    print('LUX on parvekkeellinen hytti yläkannella.')
+elif Hyttiluokka == 'A':
+    print('A on ikkunallinen hytti autokannen yläpuolella.')
+elif Hyttiluokka == 'B':
+    print('B on ikkunaton hytti autokannen yläpuolella.')
+elif Hyttiluokka == 'C':
+    print('C on ikkunaton hytti autokannen alapuolella')
+else:
+    print('Virheellinen hyttiluokka.')
+print('Kiitos ohjelman käytöstä.')
