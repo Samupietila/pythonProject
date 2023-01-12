0586d6e36777344c59826ec352567a0533947ed9
Samupietila <122264320+Samupietila@users.noreply.github.com>
2023-01-12 11:25:50 +0200

    Moduuli 4 tehtäviä

---
+++ T4_6.py
@@ -0,0 +1,9 @@
+import math
+#A = Yksikköympyrä
+#B = pienin mahdollinen ympyrä yksikköympyrän ympärille, eli sivut ovat arvoltaan 2
+#N = Pisteiden kokonaismäärä
+#n = ympyrän sisälle jäävien pisteiden kokonaismäärä
+#Pii≈4n/N
+#y = arvottu
+print('Tämä ohjelma toteuttaa algoritmin π lukuarvon laskemiseksi.')
+
