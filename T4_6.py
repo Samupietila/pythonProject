f5c96eb08d9ad6f5335e4f75768056d2560d69b5
Samupietila <122264320+Samupietila@users.noreply.github.com>
2023-01-13 15:14:12 +0200

    Moduuli 4 tehtävä

--- T4_6.py
+++ T4_6.py
@@ -1,9 +1,16 @@
-import math
-#A = Yksikköympyrä
-#B = pienin mahdollinen ympyrä yksikköympyrän ympärille, eli sivut ovat arvoltaan 2
-#N = Pisteiden kokonaismäärä
-#n = ympyrän sisälle jäävien pisteiden kokonaismäärä
-#Pii≈4n/N
-#y = arvottu
+import random
 print('Tämä ohjelma toteuttaa algoritmin π lukuarvon laskemiseksi.')
-
+N = int(input('Anna arvottavien pisteiden määrä: '))
+arvotut_pisteet = 0
+n = 0
+while arvotut_pisteet != N:
+    X = random.uniform(-1, 1)
+    Y = random.uniform(-1, 1)
+    if (X ** 2) + (Y ** 2) < 1:
+        n = n + 1
+    else:
+        n = n
+    arvotut_pisteet = arvotut_pisteet + 1
+Likiarvo = 4 * n / N
+print('Piin likiarvo algoritmilla laskettuna on:', Likiarvo)
+print('Kiitos ohjelman käytöstä.')
