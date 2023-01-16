edbbde2b21f3eb588a8c0965222a88b49b100e5c
Samupietila <122264320+Samupietila@users.noreply.github.com>
2023-01-16 16:01:36 +0200

    Add files via upload

---
+++ Moduuli4/T4_2.py
@@ -0,0 +1,12 @@
+print('Tämä ohjelma muuntaa annetut tuumat senttimetreiksi.')
+print('Negatiivinen luku lopettaa ohjelman.')
+while True:
+    Luku = float(input('Anna senttimetreiksi muunnettavat tuumat: '))
+    if Luku > 0:
+        Luku = Luku * 2.54
+        print('Annettu määrä tuumia on', Luku, 'senttimetriä.')
+    else:
+        print('Annoit negatiivisen luvun, ohjelma päättyy.')
+        break
+
+
