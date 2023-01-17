efcea0d1da11a6245d1d08722574921ff4cce940
Samupietila <122264320+Samupietila@users.noreply.github.com>
2023-01-17 15:59:06 +0200

    Update T4_5.py

--- Moduuli4/T4_5.py
+++ Moduuli4/T4_5.py
@@ -1,11 +1,14 @@
 print('Tämä ohjelma kysyy käyttäjältä käyttäjätunnuksen ja salasanan.')
 Kayttajanimi = 'python'
 Salasana = 'rules'
-while True:
+Yritykset = 0
+while Yritykset < 5:
     Annettu_kn = input('Anna käyttäjänimi: ')
     Annettu_ss = input('Anna salasana: ')
+
     if Annettu_kn == Kayttajanimi and Annettu_ss == Salasana:
         print('Tervetuloa!')
         break
     else:
         print('Pääsy evätty.')
+    Yritykset = Yritykset + 1
