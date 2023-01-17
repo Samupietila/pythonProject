a1224827810b2fdd52572547228d9543f65627ac
Samupietila <122264320+Samupietila@users.noreply.github.com>
2023-01-17 15:31:49 +0200

    Moduuli 6 tehtävä

---
+++ T6.4.py
@@ -0,0 +1,8 @@
+def listansummaaja(kehote):
+    summa = sum(kehote)
+    return summa
+def paaohjelma():
+    luvut = [1, 2, 3]
+    summatutluvut = listansummaaja(luvut)
+    print('listan lukujen summa on',summatutluvut)
+paaohjelma()
