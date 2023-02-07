daad4f875f4af2655c3bbc24def2adb7560ddede
Samupietila <122264320+Samupietila@users.noreply.github.com>
2023-02-07 13:39:22 +0200

    Ohjelmointi tehtäviä

--- Moduuli8/T8.1.py
+++ Moduuli8/T8.1.py
@@ -1,8 +1,8 @@
 import mysql.connector
 yhteys = mysql.connector.connect(host='127.0.0.1', port= 3306, database= 'lentopeli', user = 'Pythonuser', password = 'SaLaSaNa', autocommit = True)
 def haeLentoasema(ICAO):
-    sql = "SELECT name, municipality from airport"
-    sql += "WHERE ident='" + ICAO + "'"
+    sql = "SELECT name, municipality from airport "
+    sql += "WHERE ident ='" + ICAO + "'"
     kursori = yhteys.cursor()
     kursori.execute(sql)
     tulos = kursori.fetchall()
